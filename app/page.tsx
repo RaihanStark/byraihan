97abc6e45635b277034893f042c3e631c0fe5aae
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2025-07-16 17:08:14 +0700

    Remove About page and related components; add new personal page with biography and avatar image.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Posts } from "./posts";
-import { getPosts } from "./get-posts";
-
-export const revalidate = 60;
-
-export default async function Home() {
-  const posts = await getPosts();
-  return <Posts posts={posts} />;
-}
