a901cd0d249df102805f383d2b2af0c221476f00
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2025-07-16 16:53:29 +0700

    Implement package-lock.json and remove Redis integration; refactor post fetching and sorting logic in the app

--- app/(post)/header.tsx
+++ app/(post)/header.tsx
@@ -1,31 +1,19 @@
 "use client";
 
 import { useSelectedLayoutSegments } from "next/navigation";
-import { useEffect, useRef } from "react";
 import { ago } from "time-ago";
-import useSWR from "swr";
 import type { Post } from "@/app/get-posts";
-
-const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 export function Header({ posts }: { posts: Post[] }) {
   const segments = useSelectedLayoutSegments();
   // segments can be:
   // date/post
   // lang/date/post
-  const initialPost = posts.find(
+  const post = posts.find(
     post => post.id === segments[segments.length - 1]
   );
-  const { data: post, mutate } = useSWR(
-    `/api/view?id=${initialPost?.id ?? ""}`,
-    fetcher,
-    {
-      fallbackData: initialPost,
-      refreshInterval: 5000,
-    }
-  );
 
-  if (initialPost == null) return <></>;
+  if (post == null) return <></>;
 
   return (
     <>
@@ -57,36 +45,7 @@ export function Header({ posts }: { posts: Post[] }) {
             {post.date} ({ago(post.date, true)} ago)
           </span>
         </span>
-
-        <span className="pr-1.5">
-          <Views
-            id={post.id}
-            mutate={mutate}
-            defaultValue={post.viewsFormatted}
-          />
-        </span>
       </p>
     </>
   );
-}
-
-function Views({ id, mutate, defaultValue }) {
-  const views = defaultValue;
-  const didLogViewRef = useRef(false);
-
-  useEffect(() => {
-    if ("development" === process.env.NODE_ENV) return;
-    if (!didLogViewRef.current) {
-      const url = "/api/view?incr=1&id=" + encodeURIComponent(id);
-      fetch(url)
-        .then(res => res.json())
-        .then(obj => {
-          mutate(obj);
-        })
-        .catch(console.error);
-      didLogViewRef.current = true;
-    }
-  });
-
-  return <>{views != null ? <span>{views} views</span> : null}</>;
 }
