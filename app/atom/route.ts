f31cb1e7409591ac7854a4f0519abe7455ec6931
Raihan Yudo Saputra <raihanyudosaputra30@gmail.com>
2025-07-16 17:26:48 +0700

    Update Atom feed with new author details and website URL, changing title, subtitle, and links to reflect the new identity.

--- app/atom/route.ts
+++ app/atom/route.ts
@@ -6,15 +6,15 @@ export async function GET() {
   return new Response(
     `<?xml version="1.0" encoding="utf-8"?>
   <feed xmlns="http://www.w3.org/2005/Atom">
-    <title>Guillermo Rauch</title>
-    <subtitle>Essays</subtitle>
-    <link href="https://rauchg.com/atom" rel="self"/>
-    <link href="https://rauchg.com/"/>
+    <title>Raihan Yudo Saputra</title>
+    <subtitle>Blogs</subtitle>
+    <link href="https://byraihan.com/atom" rel="self"/>
+    <link href="https://byraihan.com/"/>
     <updated>${posts[0].date}</updated>
-    <id>https://rauchg.com/</id>
+    <id>https://byraihan.com/</id>
     <author>
-      <name>Guillermo Rauch</name>
-      <email>rauchg@gmail.com</email>
+      <name>Raihan Yudo Saputra</name>
+      <email>raihan@raihansoftware.com</email>
     </author>
     ${posts.slice(0, max).reduce((acc, post) => {
       const dateMatch = post.date.match(/\d{4}/);
@@ -23,7 +23,7 @@ export async function GET() {
         <entry>
           <id>${post.id}</id>
           <title>${post.title}</title>
-          <link href="https://rauchg.com/${dateMatch[0]}/${post.id}"/>
+          <link href="https://byraihan.com/${dateMatch[0]}/${post.id}"/>
           <updated>${post.date}</updated>
         </entry>`;
     }, "")}
